b02a2d1421faa720aa26b83fa83a84cea3d373a3
Abhijit Jadhav <abhijit.jadhav1998@gmail.com>
2022-05-03 20:15:51 +0530

    added some Testing code

--- unit-testing/src/test/java/com/abhijit/unittesting/business/MockList.java
+++ unit-testing/src/test/java/com/abhijit/unittesting/business/MockList.java
@@ -8,27 +8,33 @@ import static org.mockito.Mockito.anyInt;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.never;
 
 class MockList {
-	
+
 	@Mock
 	List mock = mock(List.class);
-	
+
 	@Test
 	void list_size() {
 		when(mock.size()).thenReturn(5);
 		assertEquals(5, mock.size());
 	}
-	
+
 	@Test
 	void list_size_return_different_value() {
 		when(mock.size()).thenReturn(5).thenReturn(15);
 		assertEquals(5, mock.size());
 		assertEquals(15, mock.size());
 	}
-	
+
 	@Test
 	void list_get_with_param() {
 		when(mock.get(0)).thenReturn("abhijit").thenReturn("jadhav");
@@ -43,5 +49,40 @@ class MockList {
 		assertEquals("jadhav", mock.get(1));
 	}
 
+	@Test
+	public void verificationBasic() {
+		String value1 = (String) mock.get(0);
+		String value2 = (String) mock.get(1);
+
+		verify(mock).get(0);
+		verify(mock, times(2)).get(anyInt());
+		verify(mock, atLeast(1)).get(anyInt());
+		verify(mock, atLeastOnce()).get(anyInt());
+		verify(mock, never()).get(2);
+	}
+
+	@Test
+	public void argumentCapture() {
+
+		mock.add("abhijit");
+
+		ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+		verify(mock).add(captor.capture());
+		assertEquals("abhijit", captor.getValue());
+	}
+	
+	@Test
+	public void multipleArgumentCapture() {
+
+		mock.add("abhijit");
+		mock.add("jadhav");
+
+
+		ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+		verify(mock,times(2)).add(captor.capture());
+		List<String> allValues = captor.getAllValues();
+		assertEquals("abhijit", allValues.get(0));
+		assertEquals("jadhav", allValues.get(1));
+	}
 
 }
