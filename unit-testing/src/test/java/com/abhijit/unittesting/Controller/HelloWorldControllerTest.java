6d77f102d28598699f3ab55de4da8bc989e2d198
Abhijit Jadhav <abhijit.jadhav1998@gmail.com>
2022-05-06 12:55:46 +0530

    added some Testing code

--- unit-testing/src/test/java/com/abhijit/unittesting/Controller/HelloWorldControllerTest.java
+++ unit-testing/src/test/java/com/abhijit/unittesting/Controller/HelloWorldControllerTest.java
@@ -1,7 +1,8 @@
 package com.abhijit.unittesting.Controller;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.abhijit.unittesting.controller.HelloWorldController;
@@ -27,6 +29,17 @@ class HelloWorldControllerTest {
 		RequestBuilder request = MockMvcRequestBuilders.get("/hello-world").accept(MediaType.APPLICATION_JSON);
 		MvcResult result = mockMvc.perform(request).andReturn();
 		assertEquals("Hello World", result.getResponse().getContentAsString());
+	}
+	
+
+	@Test
+	public void helloWorld_matcher() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.get("/hello-world").accept(MediaType.APPLICATION_JSON);
+		MvcResult result = mockMvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().string("Hello World"))
+				.andReturn();
+		//assertEquals("Hello World", result.getResponse().getContentAsString());
 	}
 
 }
